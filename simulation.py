f2d315dc6d8b55661f4da0be3dcfa700a36c33cf
Ben Imadali <benjithedalilama@users.noreply.github.com>
2018-01-14 18:42:18 +0530

    fixed n_floors variable naming

--- simulation.py
+++ simulation.py
@@ -7,13 +7,13 @@ def main():
     num_passengers = int(input("How many passengers does the building have?"))
     num_floors = int(input("How many floors does the building have?"))
     building = Building(num_passengers, num_floors)
-    elevator = Elevator(building.num_floors)
+    elevator = Elevator(num_floors)
     call_list = []
     for i in xrange(num_passengers):
-        start_floor = random.choice(xrange(elevator.num_floors))
-        destination_floor = random.choice(xrange(elevator.num_floors))
+        start_floor = random.choice(xrange(elevator.n_floors))
+        destination_floor = random.choice(xrange(elevator.n_floors))
         while start_floor == destination_floor:
-            destination_floor = random.choice(xrange(elevator.num_floors))
+            destination_floor = random.choice(xrange(elevator.n_floors))
         passenger = Passenger(start_floor, destination_floor)
         call_list.append(passenger)
 
