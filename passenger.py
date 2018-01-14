8d52cff97480d5dc3290984951b6d0f0009505c1
Guilherme <guilherme@minerva.kgi.edu>
2018-01-14 17:47:41 +0530

    change passenger class to add destination and remove weight

--- passenger.py
+++ passenger.py
@@ -1,11 +1,24 @@
 class Passenger(object):
 
-    def __init__(self, current_floor, target_floor, weight = 60, distance_traveled):
+    def __init__(self, current_floor, destination):
         self.current_floor = current_floor
-        self.target_floor = target_floor
-        self.weight = weight
+        self.destination = destination
+        self.direction = direction(current_floor, destination) ## to do
         self.time_cost = 0
 
     def increment_timecost(self, cost = 1):
         self.time_cost += cost
-        
+
+    def direction(current_floor, destination):
+        # calculate the direction of movement based on start and end floors
+        direction = None
+        movement = destination - current_floor
+        # calculate floors difference to determine direction:
+        if movement > 0:
+            direction = +1
+        elif movement < 0:
+            direction = -1
+        else:
+            # if destination is same as current floor, stay
+            direction = 0
+        return direction
